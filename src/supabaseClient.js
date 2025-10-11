91e6d7775a2b1725cefe1a0989b686c3fa2dc9a2
Maikel <maikrp@gmail.com>
2025-10-11 09:50:43 -0600

    fix: move console.log below import

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
-console.log("🔍 Variables de entorno:", import.meta.env);
-
-
 import { createClient } from "@supabase/supabase-js";
+
+console.log("🔍 Variables de entorno:", import.meta.env);
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
